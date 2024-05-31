85e74f483478ef78f0cdb06c381db619c2fa7c19
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-31 13:32:10 +0545

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = () =>{
                 <div className="shopping">
                     <p>Also available on</p>
                     <div className="icons">
-                        <img src="/src/assets/images/flipkart.png" alt="flipkart-logo" />
-                        <img src="/src/assets/images/amazon.png" alt="amazon-logo" />
+                        <img src="/images/flipkart.png" alt="flipkart-logo" />
+                        <img src="/images/amazon.png" alt="amazon-logo" />
                     </div>
                 </div>
             </div>
             <div className="hero-image">
-                <img src="/src/assets/images/shoe_image.png" alt="hero-image" />
+                <img src="/images/shoe_image.png" alt="hero-image" />
             </div>
         </main>
     )
